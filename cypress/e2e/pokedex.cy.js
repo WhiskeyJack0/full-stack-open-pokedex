2b358255493452d0a259725430cbafbb67b48ef7
WhiskeyJack0 <whiskyjack0@protonmail.com>
2022-11-11 16:18:21 +0530

    Add another cypress test

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -3,5 +3,13 @@ describe('Pokedex', function() {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  }),
+  it('pokemon page can be navigated to', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('squirtle').click()
+    cy.location('pathname', { timeout: 60000 })
+      .should('include', '/squirtle')
+    cy.contains('rain dish')
   })
+
 })
