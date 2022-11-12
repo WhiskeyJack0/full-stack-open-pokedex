77964d8e53484a43e74ac092bc473c7bc14e940d
WhiskeyJack0 <whiskyjack0@protonmail.com>
2022-11-12 17:41:59 +0530

    Add custom health check

---
+++ health_check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Hello from shell script"
+apt-get update; apt install -y curl
+response=$(curl --silent localhost:5000/health)
+
+echo "$response"
+if [[ "$response" != "ok" ]]; then
+  exit 1
+else
+  exit 0
+fi
