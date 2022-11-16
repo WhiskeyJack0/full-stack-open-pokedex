a508c36e87f062d8adc305bb67b15fe32c537ef0
WhiskeyJack0 <whiskyjack0@protonmail.com>
2022-11-16 10:50:42 +0530

    update execute permissions for health check script

--- health_check.sh
+++ health_check.sh
@@ -5,8 +5,10 @@ apt-get update; apt install -y curl
 response=$(curl --silent localhost:5000/health)
 
 echo "Server response: $response"
-if [[ "$response" != "ok" ]]; then
-  exit 1
-else
+if [[ "$response" == "ok" ]]; then
+  echo "Successful response from /health"
   exit 0
+else
+  echo "Failed query to /health"
+  exit 1
 fi
