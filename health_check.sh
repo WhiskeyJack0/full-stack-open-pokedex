6ec3e95ab10392052ee2e9e1b30a77032b2411ca
WhiskeyJack0 <whiskyjack0@protonmail.com>
2022-11-16 10:07:16 +0530

    try debug for failing health check script

--- health_check.sh
+++ health_check.sh
@@ -4,7 +4,7 @@ echo "Hello from shell script"
 apt-get update; apt install -y curl
 response=$(curl --silent localhost:5000/health)
 
-echo "$response"
+echo "Server response: $response"
 if [[ "$response" != "ok" ]]; then
   exit 1
 else
